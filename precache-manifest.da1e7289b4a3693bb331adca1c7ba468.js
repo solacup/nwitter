2b616ac6982c8fdd90fe31a9bf1e3e4f08be74b7
Solacup <solacup07@naver.com>
2020-09-21 16:54:52 +0900

    Updates

---
+++ precache-manifest.da1e7289b4a3693bb331adca1c7ba468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5deae5005636d9f80054d82251e263c",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "dfb7542faf42ebbaea19",
+    "url": "/nwitter/static/css/main.9333dc4c.chunk.css"
+  },
+  {
+    "revision": "e0e99d5ce75c288fd1c6",
+    "url": "/nwitter/static/js/2.3c574183.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.3c574183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb7542faf42ebbaea19",
+    "url": "/nwitter/static/js/main.9583b12a.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
